b7ee6690bc9e9c59a87477cf8da655c438a45a82
shizi <lan.zhu.123@163.com>
2021-08-30 22:03:40 +0800

    init 初始化项目

---
+++ Matcher.go
@@ -0,0 +1,23 @@
+package mikilin
+
+// todo
+func check(object interface{}) bool {
+	// todo
+	return true
+}
+
+//
+func checkWithField(object interface{}, field ...string) bool {
+	// todo
+	return true
+}
+
+func validate(object interface{}) (string, error) {
+	// todo
+	return "", nil
+}
+
+func validateWithField(object interface{}, field ...string) (string, error) {
+	// todo
+	return "", nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SimonAlong/Mikilin-go
+
+go 1.12
