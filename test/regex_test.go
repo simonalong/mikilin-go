07e46d223745b19849f83ad5ebab5b7c56ea4ede
shizi <lan.zhu.123@163.com>
2021-09-03 15:51:45 +0800

    update

--- test/regex_test.go
+++ test/regex_test.go
@@ -16,7 +16,7 @@ func TestRegex(t *testing.T) {
 	var result bool
 	var err string
 
-	//测试 正常情况
+	// 测试 正常情况
 	value = ValueRegexEntity{Name: "zhouOKzhen"}
 	result, err = mikilin.Check(value, "name")
 	assert.TrueErr(t, result, err)
@@ -26,7 +26,7 @@ func TestRegex(t *testing.T) {
 	result, err = mikilin.Check(value, "age")
 	assert.TrueErr(t, result, err)
 
-	//测试 异常情况
+	// 测试 异常情况
 	value = ValueRegexEntity{Name: "chenzhen"}
 	result, err = mikilin.Check(value, "name")
 	assert.Equal(t, "核查错误：属性 Name 的值 chenzhen 没命中只允许的正则表达式 ^zhou.*zhen$ ", err, result, false)
