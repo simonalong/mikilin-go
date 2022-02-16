ff86fd07c3658bdfb7c49138987f4c6656805bf3
shizi <lan.zhu.123@163.com>
2022-02-16 14:12:47 +0800

    update 更新goid的获取方式

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/antonmedv/expr v1.9.0
-	github.com/go-eden/routine v0.0.3
+	github.com/isyscore/go-goid v0.0.1
 	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- mikilin.go
+++ mikilin.go
@@ -3,7 +3,7 @@ package mikilin
 import (
 	"fmt"
 	"github.com/antonmedv/expr"
-	"github.com/go-eden/routine"
+	"github.com/isyscore/go-goid"
 	"github.com/simonalong/mikilin-go/constant"
 	matcher "github.com/simonalong/mikilin-go/match"
 	"github.com/simonalong/mikilin-go/util"
@@ -81,7 +81,7 @@ func Check(object interface{}, fieldNames ...string) (bool, string) {
 			}
 
 			// 核查结果：任何一个属性失败，则返回失败
-			routine.Go(func() {
+			goid.Go(func() {
 				check(object, field, fieldValue.Interface(), ch)
 			})
 			checkResult := <-ch
@@ -136,7 +136,7 @@ func Check(object interface{}, fieldNames ...string) (bool, string) {
 			}
 
 			// 核查结果：任何一个属性失败，则返回失败
-			routine.Go(func() {
+			goid.Go(func() {
 				go check(object, field, fieldValue.Interface(), ch)
 			})
 			checkResult := <-ch
